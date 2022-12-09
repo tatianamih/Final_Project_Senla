3d2efde41711afb75da38fda427186b239800d03
tatianamih <67430904+tatianamih@users.noreply.github.com>
2022-12-09 19:00:14 +0300

    add module security

---
+++ ElectricScooterRentalService/security/src/main/java/com/courses/senla/jwt/AuthEntryPointJwt.java
@@ -0,0 +1,39 @@
+package com.courses.senla.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+@Log4j2
+@Component
+public class AuthEntryPointJwt implements AuthenticationEntryPoint {
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException authException) throws IOException, ServletException {
+		log.error(authException.getMessage());
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+
+		final Map<String, Object> body = new HashMap<>();
+		body.put("status", HttpServletResponse.SC_FORBIDDEN);
+		body.put("error", "Unauthorized");
+		body.put("message", authException.getMessage());
+		body.put("path", request.getServletPath());
+
+		final ObjectMapper mapper = new ObjectMapper();
+		mapper.writeValue(response.getOutputStream(), body);
+	}
+
+}
